105803a66dc0b9395d3a7e383c542edd38b3f16c
翟硕 <zhaishuo@100tal.com>
2019-08-30 10:47:36 +0800

    add: 添加基础文件

---
+++ java-web-demo/src/main/java/com/zs/demo/Test.java
@@ -0,0 +1,14 @@
+package com.zs.demo;/**
+ * Talcloud.com Inc.
+ * Copyright (c) 2018-2020 All Rights Reserved.
+ */
+
+/**
+ * <pre>
+ *    描述信息
+ * </pre>
+ *
+ * @version $Id: Test.java, v1.0 2019/8/30 10:43 86130 Exp $
+ */
+public class Test {
+}
---
+++ java-web-demo/src/test/java/com/zs/dome/feature/TestDao.java
@@ -0,0 +1,14 @@
+package com.zs.dome.feature;/**
+ * Talcloud.com Inc.
+ * Copyright (c) 2018-2020 All Rights Reserved.
+ */
+
+/**
+ * <pre>
+ *    描述信息
+ * </pre>
+ *
+ * @version $Id: TestDao.java, v1.0 2019/8/30 10:46 86130 Exp $
+ */
+public class TestDao {
+}
